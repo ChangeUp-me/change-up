b6bab1a45e68ad2e7dc02af6009ea99079c2bbc9
Alex D'Amico <alex@checkmatecreations.com>
2015-12-01 12:01:51 -0500

    Created site structure

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -10,3 +10,45 @@ Router.route('/', {
   controller: 'HomeController',
   where: 'client'
 });
+
+Router.route('shop', {
+  name: 'shop',
+  controller: 'ShopController',
+  where: 'client'
+});
+
+Router.route('charities', {
+  name: 'charities',
+  controller: 'CharitiesController',
+  where: 'client'
+});
+
+Router.route('about', {
+  name: 'about',
+  controller: 'AboutController',
+  where: 'client'
+});
+
+Router.route('news', {
+  name: 'news',
+  controller: 'NewsController',
+  where: 'client'
+});
+
+Router.route('account', {
+  name: 'account',
+  controller: 'AccountController',
+  where: 'client'
+});
+
+Router.route('cart', {
+  name: 'cart',
+  controller: 'CartController',
+  where: 'client'
+});
+
+Router.route('checkout', {
+  name: 'checkout',
+  controller: 'CheckoutController',
+  where: 'client'
+});
