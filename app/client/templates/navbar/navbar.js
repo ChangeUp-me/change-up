2acd85b1c6a1ff51ca21cf1fa1f4e654c0d78289
Alex D'Amico <alex@checkmatecreations.com>
2015-12-01 13:48:38 -0500

    Off-canvas navigation working

--- app/client/templates/navbar/navbar.js
+++ app/client/templates/navbar/navbar.js
@@ -17,6 +17,23 @@ Template.Navbar.onCreated(function () {
 });
 
 Template.Navbar.onRendered(function () {
+	$(function() {
+		$('.toggle-nav').click(function() {
+			// Calling a function in case you want to expand upon this.
+			toggleNav();
+		});
+	});
+
+	function toggleNav() {
+		if ($('#site-wrapper').hasClass('show-nav')) {
+			// Do things on Nav Close
+			$('#site-wrapper').removeClass('show-nav');
+		} else {
+			// Do things on Nav Open
+			$('#site-wrapper').addClass('show-nav');
+		}
+		//$('#site-wrapper').toggleClass('show-nav');
+	}
 });
 
 Template.Navbar.onDestroyed(function () {
