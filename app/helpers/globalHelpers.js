bd8433cd906a8ff9a561a911af0f8996fd41782e
MehdiSolati <mkslt04@gmail.com>
2016-01-08 14:55:11 -0500

    Wake up in the mornin' feelin' like P-Diddy (hey what's up girl) Grab my glasses I'm out the door I'm gonna hit the city (let's go) Before I leave brush ma teeth with a bottle of Jack 'Cause when I leave for the night I ain't comin' back
    I'm talking pedicure on our toes toes
    Trying on all our clothes clothes
    Boys blowin' up our phones phones
    
    Drop top and playin' our favorite CD's
    Pullin' up to the parties
    Tryna get a little bit tipsy
    
    [CHORUS]
    Don't stop, make it pop
    DJ, blow my speakers up
    Tonight, Imma fight
    'Til we see the sunlight
    TiK ToK, on the clock
    But the party don't stop no
    Whoa-oh oh oh
    Whoa-oh oh oh
    
    Don't stop, make it pop
    DJ, blow my speakers up
    Tonight, Imma fight
    'Til we see the sunlight
    TiK ToK, on the clock
    But the party don't stop no
    Whoa-oh oh oh
    Whoa-oh oh oh
    
    Ain't got a care in world, but got plenty of beer
    Ain't got no money in my pocket, but I'm already here
    Now, the dudes are lining up cause they hear we got swagger
    But we kick em to the curb unless they look like Mick Jagger
    
    I'm talkin' bout - everybody getting crunk, crunk
    Boys tryna touch my junk, junk
    Gonna smack him if he getting too drunk, drunk
    Now, now - we goin' 'til they kick us out, out
    Or the police shut us down, down
    Police shut us down, down
    Po-po shut us - (down)-man
    
    [CHORUS]
    Don't stop, make it pop
    DJ, blow my speakers up
    Tonight, Imma fight
    'Til we see the sunlight
    TiK ToK, on the clock
    But the party don't stop no
    Whoa-oh oh oh
    Whoa-oh oh oh
    
    Don't stop, make it pop
    DJ, blow my speakers up
    Tonight, Imma fight
    'Til we see the sunlight
    TiK ToK, on the clock
    But the party don't stop no
    Whoa-oh oh oh
    Whoa-oh oh oh
    
    [BRIDGE]
    DJ, You build me up
    You break me down
    My heart, it pounds
    Yeah, you got me
    With my hands up
    You got me now
    You got that sound
    Yeah, you got me
    
    You build me up
    You break me down
    My heart, it pounds
    Yeah, you got me
    With my hands up
    Put your hands up
    Put your hands up
    
    Now, the party don't start 'til I walk in
    
    [CHORUS]
    Don't stop, make it pop
    DJ, blow my speakers up
    Tonight, Imma fight
    'Til we see the sunlight
    TiK ToK, on the clock
    But the party don't stop no
    Whoa-oh oh oh
    Whoa-oh oh oh
    
    Don't stop, make it pop
    DJ, blow my speakers up
    Tonight, Imma fight
    'Til we see the sunlight
    TiK ToK, on the clock
    But the party don't stop no
    Whoa-oh oh oh
    Whoa-oh oh oh

--- app/helpers/globalHelpers.js
+++ app/helpers/globalHelpers.js
@@ -1,7 +1,19 @@
 Handlebars.registerHelper('noOfReviews', function (reviews) {
+  console.log(reviews);
+  if (reviews === undefined) {
+    return "0 reviews";
+  } else if (reviews.length === 1 ) {
+    return (reviews.length +" review");
+  } else {
+    return (reviews.length +" reviews");
+  }
+});
+
+Handlebars.registerHelper('noOfReviewsNoText', function (reviews) {
+  console.log(reviews);
   if (reviews === undefined) {
     return "0";
   } else {
-    return reviews.length;
+    return (reviews.length);
   }
 });
