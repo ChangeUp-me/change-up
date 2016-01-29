36da5900c939b59bca434ca3d7f4ccba8ae273df
Alex D'Amico <alex@checkmatecreations.com>
2016-01-29 16:02:29 -0500

    Desktop Fulfillment and Product pages, Product mobile UI update, style and layout edits, share button pointer, removed extraneous edit buttons

--- app/client/templates/item/item.js
+++ app/client/templates/item/item.js
@@ -7,6 +7,9 @@ Template.Item.events({
 			productName.set(this.product.name);
 			productDescription.set(this.product.description);
 	    $('#shareModal').modal('show');
+	},
+	"click .size-select li" : function (event) {
+		$(event.target).toggleClass('selected');
 	}
 });
 
@@ -14,7 +17,29 @@ Template.Item.events({
 /* Item: Helpers */
 /*****************************************************************************/
 Template.Item.helpers({
+	charities : function () {
+		return this.charities;
+	},
 
+	sizes : function () {
+		var sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+		var selfSizes = this.sizes;
+
+		//if the user has sizes set make them uppercase
+		if(selfSizes) {
+			selfSizes = selfSizes.map(function (size) {
+			return size.toUpperCase();
+		})
+	}
+
+	sizes = sizes.map(function (size) {
+		if(selfSizes && selfSizes.indexOf(size) > -1)
+			return {size : size, selected : true};
+		else
+			return {size : size, selected : false};
+		})
+		return sizes;
+	}
 });
 
 /*****************************************************************************/
