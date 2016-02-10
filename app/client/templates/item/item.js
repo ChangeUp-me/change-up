c5b0cc125e979131b67798747dd8364929faee2d
Michael Solati <mkslt04@gmail.com>
2016-02-10 10:36:22 -0500

    Adding items to cart works well, takes charity id, only allows one size, it gooooooood

--- app/client/templates/item/item.js
+++ app/client/templates/item/item.js
@@ -3,13 +3,53 @@
 /*****************************************************************************/
 Template.Item.events({
 	'click .openShareWidget': function(){
-			productId.set(this.product._id);
-			productName.set(this.product.name);
-			productDescription.set(this.product.description);
-	    $('#shareModal').modal('show');
+		productId.set(this.product._id);
+		productName.set(this.product.name);
+		productDescription.set(this.product.description);
+		$('#shareModal').modal('show');
 	},
-	"click .size-select li" : function (event) {
+	// "click .size-select li" : function (event) {
+	// 	$(event.target).toggleClass('selected');
+	// },
+	"click .size-select li" : function (event, template) {
+		var allSizes= [];
+		for (var i = 0; i < template.data.product.sizes.length; i++) {
+			allSizes.push("size"+template.data.product.sizes[i])
+		}
+		$('#' + allSizes.join(',#')).removeClass('selected');
+		$('#' + allSizes.join(',#')).data('value', '');
 		$(event.target).toggleClass('selected');
+		$(event.target).data('value', 'selected');
+	},
+	'click .add-to-cart-btn': function(event, template){
+		event.preventDefault();
+		var selectedSize;
+
+		var quantity = template.find('#quantity').value;
+		var charity = template.find('#charities').value;
+		for (var i = 0; i < this.product.sizes.length; i++) {
+			if ($("#size"+this.product.sizes[i]).data('value') === "selected"){
+				selectedSize = this.product.sizes[i];
+			}
+		}
+
+		var cartItem = {
+			productId : this.product._id,
+			size : selectedSize,
+			quantity : parseInt(quantity),
+			image : this.product.image,
+			charityId: charity
+		};
+
+		if (cartItem.size === undefined){
+			sAlert.error('select a size');
+		} else if (cartItem.quantity < 1) {
+			sAlert.error('select a quantity more than 1');
+		} else if (isNaN(cartItem.quantity)) {
+			sAlert.error('select a quantity');
+		} else {
+			CART.addItem(cartItem);
+		}
 	}
 });
 
