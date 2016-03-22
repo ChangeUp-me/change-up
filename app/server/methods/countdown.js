afc27d78907e2d0cbfd2b374681b1af2622bfdbd
Michael Solati <mkslt04@gmail.com>
2016-03-22 11:51:45 -0400

    Change up the function to calculate the top products

--- app/server/methods/countdown.js
+++ app/server/methods/countdown.js
@@ -44,57 +44,80 @@ Meteor.startup(function () {
         console.log("Updating Featured Products");
         FeaturedProducts.update({}, {$set: {'current': false}}, {multi: true}); //update all items in collection to not be current day
 
-        var possibleVendorList = Vendors.find().fetch(); //get length of all vendors in store
-        if (possibleVendorList.length < 6){ //if all vendors in changeup less than 6 set no of vendors to select to no of vendors in store
-          var noOfVendors = possibleVendorList.length;
-        } else { // if more than 6, set no of vendors to 6
-          var noOfVendors = 6;
-        }
+        var possibleVendorList = Vendors.find().fetch(); //get all vendors in store
+        possibleVendorList = shuffle(possibleVendorList);
+        var arrayOfObjects = [];
+        var i = 0;
 
-        var arrayOfVendors = []; // holder for the place in the array of vendors well select from
-        for (var i = 0; i < noOfVendors; i){
-          var randomlySelectedObj = Math.floor((Math.random()*possibleVendorList.length)); //randomly select a number between 0 and no of products in shop
-          var inArray = _.contains(arrayOfVendors, (possibleVendorList[randomlySelectedObj]._id)); // boolean to see if id already selected
-          if (inArray === false){ // if id not in array
-            arrayOfVendors.push(possibleVendorList[randomlySelectedObj]._id); //insert into array
-            i++; // increment
-          }
-        }
-
-        var arrayOfObjects = []; // holder for the place in the array of products well select from
-        for (var i = 0; i < arrayOfVendors.length; i++){
+        while (arrayOfObjects.length < 6 && i < possibleVendorList.length) {
           try {
-            var vendorsItems = Products.find({'vendorId':arrayOfVendors[i]}).fetch();
-            var randomlySelectedObj = Math.floor((Math.random()*newProducts.length)); //randomly select a number between 0 and no of products in shop
-            arrayOfObjects.push(vendorsItems[randomlySelectedObj]._id); //insert into array
+            var newProducts = Products.find({"vendorId": possibleVendorList[i]._id}).fetch();
+            var randomlySelectedObj = Math.floor((Math.random()*newProducts.length));
+            var newProduct = newProducts[randomlySelectedObj]._id;
+            arrayOfObjects.push(newProduct);
           } catch (e) {
 
           }
-
+          i++;
         }
+
         FeaturedProducts.insert({'date': end, 'products': arrayOfObjects, 'current': true});
       }
     } catch (e) {
       console.log("First Time Featured Products");
-      var newProducts = Products.find().fetch(); //get length of all products in store
-      if (newProducts.length < 6){ //if all products in store less than 6 set no of items to select to no of items in store
-        var noOfProducts = newProducts.length;
-      } else { // if more than 6, set no of products to 6
-        var noOfProducts = 6;
-      }
-      var arrayOfObjects = []; // holder for the place in the array of products well select from
-      for (var i = 0; i < noOfProducts; i){
-        var randomlySelectedObj = Math.floor((Math.random()*newProducts.length)); //randomly select a number between 0 and no of products in shop
-        var inArray = _.contains(arrayOfObjects, (newProducts[randomlySelectedObj]._id)); // boolean to see if id already selected
-        if (inArray === false){ // if id not in array
-          arrayOfObjects.push(newProducts[randomlySelectedObj]._id); //insert into array
-          i++; // increment
+      // var newProducts = Products.find().fetch(); //get length of all products in store
+      // if (newProducts.length < 6){ //if all products in store less than 6 set no of items to select to no of items in store
+      //   var noOfProducts = newProducts.length;
+      // } else { // if more than 6, set no of products to 6
+      //   var noOfProducts = 6;
+      // }
+      // var arrayOfObjects = []; // holder for the place in the array of products well select from
+      // for (var i = 0; i < noOfProducts; i){
+      //   var randomlySelectedObj = Math.floor((Math.random()*newProducts.length)); //randomly select a number between 0 and no of products in shop
+      //   var inArray = _.contains(arrayOfObjects, (newProducts[randomlySelectedObj]._id)); // boolean to see if id already selected
+      //   if (inArray === false){ // if id not in array
+      //     arrayOfObjects.push(newProducts[randomlySelectedObj]._id); //insert into array
+      //     i++; // increment
+      //   }
+      // }
+      var possibleVendorList = Vendors.find().fetch(); //get length of all vendors in store
+      possibleVendorList = shuffle(possibleVendorList);
+      var arrayOfObjects = [];
+      var i = 0;
+
+      while (arrayOfObjects.length < 6 && i < possibleVendorList.length) {
+        try {
+          var newProducts = Products.find({"vendorId": possibleVendorList[i]._id}).fetch();
+          var randomlySelectedObj = Math.floor((Math.random()*newProducts.length));
+          var newProduct = newProducts[randomlySelectedObj]._id;
+          arrayOfObjects.push(newProduct);
+        } catch (e) {
+
         }
+        i++;
       }
       FeaturedProducts.insert({'date': end, 'products': arrayOfObjects, 'current': true});
     }
   }
   // Check to update featured products on a hourly basis
-  // Meteor.setInterval(updateFeaturedProducts, 3600000);
-  Meteor.setInterval(updateFeaturedProducts, 3600);
+  Meteor.setInterval(updateFeaturedProducts, 3600000);
 });
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
