b0f70deeb26dc866d8701a695f11ca7ea60be8bf
Connor Croteau <connor@checkmatecreations.com>
2016-04-04 17:31:32 -0400

    added geoff personal email

--- server/emailServer.js
+++ server/emailServer.js
@@ -2,7 +2,7 @@ Meteor.startup(function () {
   ContactUs.before.insert(function(userId, message) {
     Email.send({
       from: message.email,
-      to: ["geoff@changeup.me", "matt@changeup.me","niksurb228@gmail.com"],
+      to: ["geoff@changeup.me", "matt@changeup.me", "niksurb228@gmail.com"],
       replyTo: message.email,
       subject: "Contact Us Submission",
       text: message.name + ' : ' + message.message
