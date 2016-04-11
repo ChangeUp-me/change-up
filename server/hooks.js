61f5e00147b8e8ea01cda875ab7109b6edea6abb
weaver305 <taweavr1@server.local>
2016-04-11 14:43:37 -0400

    fix onboarding vendor email

--- server/hooks.js
+++ server/hooks.js
@@ -2,21 +2,21 @@
 	//send a newly created vendor an email
 	Meteor.users.after.update(function (userId, doc, fieldNames, modifier, options) {
 		var updateObj = modifier.$set;
-		var user = Meteor.users.findOne(doc._id);
-		// SENDS FROM WRONG EMAIL
-		// try {
-		// 	if(_.isObject(updateObj) && _.isArray(updateObj.roles)) {
-		// 		if(updateObj.roles.indexOf('vendor') > -1) {
-		// 			Email.send({
-		// 				to : user.emails[0].address,
-		// 				from : 'hello@changeup.me',
-		// 				subject : 'Setup Your Vendor Account!',
-		// 				text : "A vendor account has been made for you on ChangeUp.me. To set up your store, login at http://www.changeup.me/login with your username and password. Then go to http://www.changeup.me/vendorProfile to create your store!"
-		// 			});
-		// 		}
-		// 	}
-		// } catch (e) {
-		// 	console.error('send-vendor-email', e.stack);
-		// }
+
+		try {
+			if(_.isObject(updateObj) && _.isArray(updateObj.roles)) {
+				if(updateObj.roles.indexOf('vendor') > -1) {
+					console.log('updateobj', doc);
+					Email.send({
+						to : doc.emails[0].address,
+						from : 'hello@changeup.me',
+						subject : 'Setup Your Vendor Account!',
+						text : "A vendor account has been made for you on ChangeUp.me. To set up your store, login at http://www.changeup.me/login with your username and password. Then go to http://www.changeup.me/vendorProfile to create your store!"
+					});
+				}
+			}
+		} catch (e) {
+			console.error('send-vendor-email', e.stack);
+		}
 	});
 })();
