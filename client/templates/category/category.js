1df46ccd227fa8b154b1a19693e5b115a3b9f4b5
weaver305 <taweavr1@server.local>
2016-06-04 16:18:34 -0400

    subcategory page and filter funcs

--- client/templates/category/category.js
+++ client/templates/category/category.js
@@ -3,17 +3,39 @@
 	/* category: Event Handlers */
 	/*****************************************************************************/
 
-	Template.category.events({});
+	Template.category.events({
+		'click [data-click-fitlersubcats]' : function (event) {
+			var subcategory = this.toString().toLowerCase();
+			var products = Session.get('original:products');
+
+			Session.set('current:subcategory', subcategory);
+
+			Session.set('visible:products', _.filter(products, function (item) {
+				var sub = item.subcategory || "";
+				return sub.toLowerCase() == subcategory;
+			}));
+		},
+		'click [data-click-showall]' : function (event) {
+			 Session.set('current:subcategory');
+			Session.set('visible:products',Session.get('original:products'));
+		}
+ 	});
 
 	/*****************************************************************************/
 	/* category: Helpers */
 	/*****************************************************************************/
 	Template.category.helpers({
 		subcategories : function () {
-			return ['One', 'Two', 'Three', 'Four', 'Five'];
+			return _.uniq(_.map(Session.get('original:products'), function (item) {
+				var sub = item.subcategory || "";
+				return sub.toLowerCase();
+			}));
 		},
 		products : function () {
-			return this.products;
+			return Session.get('visible:products');
+		},
+		selected : function () {
+			return this.toString() == Session.get('current:subcategory');
 		}
 	});
 
@@ -22,7 +44,9 @@
 	/*****************************************************************************/
 	Template.category.onCreated(function () {});
 
-	Template.category.onRendered(function () {});
+	Template.category.onRendered(function () {
+	});
 
-	Template.category.onDestroyed(function () {});
+	Template.category.onDestroyed(function () {
+	});
 })();
